a8a0bedd79656425494363adbe116078bfadb041
oRichard05 <everettbakersco@gmail.com>
2025-03-02 00:37:49 -0500

    V0.4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Terminal from "./pages/Terminal";
 import OS from "./pages/OS";
 
@@ -6,6 +6,7 @@ function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/terminal" />} />
                 <Route path="/terminal" element={<Terminal />} />
                 <Route path="/OS" element={<OS />} />
             </Routes>
