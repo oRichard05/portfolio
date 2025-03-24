ecf29dd7646ea454593c4c352a2fcb18d24df14f
EBC <everettbakersco@gmail.com>
2025-03-24 01:28:52 -0400

    V1.7

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/AboutMe.css';
-import {FaTimes} from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 const AboutMe = ({ closeApp }) => {
     const [position, setPosition] = useState({ x: 100, y: 100 });
@@ -27,7 +27,6 @@ const AboutMe = ({ closeApp }) => {
         setZIndex(prevZIndex => prevZIndex + 1); // Increase zIndex to bring window to the front
     };
 
-    // Handle dragging logic on mouse events
     useEffect(() => {
         const handleMouseMoveListener = (e) => handleMouseMove(e);
         const handleMouseUpListener = () => handleMouseUp();
@@ -67,33 +66,32 @@ const AboutMe = ({ closeApp }) => {
                     <div className="about-text">
                         <h2>Otoni Richard</h2>
                         <p>
-                            I am a Brazilian student pursuing a double major in
-                            Computer Science and Economics at UNC Chapel Hill.
-                            My passion for technology began in high school, during the pandemic,
-                            when I started learning to program and build websites.
+                            I’m a Brazilian student currently studying Computer Science and Economics at UNC Chapel Hill. I grew up in the interior of São Paulo, Brazil, and my interest in technology and programming started back in high school, right around the time the pandemic hit.
                         </p>
                     </div>
                 </div>
 
                 <div className="about-body">
                     <p>
-                        In 2022 and 2023, I joined my school's robotics team, developing a line-following robot
-                        and competing in the Brazilian Robotics Olympiad (OBR). Our team won regional, state,
-                        and national titles in the Best Programming category.
-                    </p>
+                        As a kid, I was always competitive, which led me to get involved in football and judo. Through those experiences, I learned a lot about teamwork, discipline, and perseverance                    </p>
+                    <p>
+                        In high school, I decided to take my love for programming and mix it with my competitive side. I became the captain of my school’s robotics team, where we built an autonomous robot that could follow lines and rescue victims. We ended up winning regional, state, and national championships at the Brazilian Robotics Olympiad (OBR), which was an amazing experience.                    </p>
+
                     <div className="about-image-group">
                         <img src="/images/foto1.png" alt="Competition" className="about-image" />
                         <img src="/images/robotica.png" alt="Robotics Team" className="about-image" />
                     </div>
 
                     <p>
-                        Beyond robotics, I have participated in several state and national programming and math competitions.
-                        In 2023, I was selected among 20 students from São Paulo to receive a full scholarship
-                        covering all expenses for four years of college in the United States.
+                        Along the way, I also participated in a bunch of math and programming competitions, especially in my senior year. Those challenges helped me sharpen my problem-solving skills and strengthened my passion for technology.
                     </p>
+
                     <p>
-                        I am eager to apply my knowledge and skills in technology to real-world challenges.
-                        I am particularly interested in web development, software engineering, and data science.
+                        Outside of school, I worked at my uncle’s store during my final year of middle school and throughout high school summers. I helped with sales and inventory, which taught me a lot about organization and communication
+                    </p>
+
+                    <p>
+                        Now, as I continue my studies, I’m excited to dive deeper into web development and software engineering. I’m focused on growing my knowledge in the tech field, especially with tools like C/C++, Python, JavaScript, HTML, CSS, React, and Figma.
                     </p>
                 </div>
             </div>
